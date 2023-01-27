73e392f467dee620233f0f965ddea0a4f828024c
lzj1769 <lzj1769@gmail.comz>
2023-01-26 20:25:59 -0500

    update docs

--- pychromvar/__init__.py
+++ pychromvar/__init__.py
@@ -1,7 +1,9 @@
+__version__ = "0.0.2"
+__version_info__ = tuple([int(num) for num in __version__.split('.')])  # noqa: F401
+
 from .add_gc_bias import add_gc_bias
 from .get_bg_peaks import get_bg_peaks
 from .match_motif import match_motif
 from .compute_deviations import compute_deviations
-#from .utils import add_peak_seq
+from .utils import add_peak_seq
 
-__version__ = "0.0.2"
