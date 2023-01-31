29a74e378f71e26d4386e7daa88e30e0b31465df
lzj1769 <lzj1769@gmail.comz>
2023-01-31 13:48:58 -0500

    update api

--- pychromvar/__init__.py
+++ pychromvar/__init__.py
@@ -1,8 +1,8 @@
 __version__ = "0.0.3"
 __version_info__ = tuple([int(num) for num in __version__.split('.')])  # noqa: F401
 
-from .preprocessing import *
-from .match_motif import *
-from .compute_deviations import *
-from .get_genome import *
+from .preprocessing import get_bg_peaks, add_gc_bias, add_peak_seq
+from .match_motif import match_motif
+from .compute_deviations import compute_deviations, compute_expectation
+from .get_genome import get_genome
 
